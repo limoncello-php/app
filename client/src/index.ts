c5de228eb6cc3b41dc265a9c02b01deb63773f24
EK <ek@neomerx.com>
2018-05-27 00:33:22 +0300

    Improve API sample.

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,56 +1,67 @@
 import 'bootstrap';
 import './../scss/index.scss';
 
-export {AuthToken} from './Application/AuthToken';
-
 // import {AuthToken} from './Application/AuthToken';
 // import {QueryBuilder} from '@limoncello-framework/json-api-client';
 // import {TokenInterface} from "@limoncello-framework/oauth-client";
 //
 // (async () => {
+//
 //     const serverUrl = 'http://localhost:8080';
 //     const userName = 'denesik.stewart@gmail.com';
 //     const password = 'secret';
 //
-//     const tokenUrl = serverUrl + '/token';
-//     const apiPrefix = serverUrl + '/api/v1';
-//
 //     try {
-//         const token: TokenInterface = await (new AuthToken(tokenUrl)).password(userName, password);
+//         // Get OAuth token (for details see https://tools.ietf.org/html/rfc6749#section-5.1)
+//         const token: TokenInterface = await (new AuthToken(serverUrl + '/token')).password(userName, password);
+//         console.log('OAuth token: ' + JSON.stringify(token, null, ' '));
+//         const authHeader = {Authorization: 'Bearer ' + token.access_token};
 //
-//         // see https://tools.ietf.org/html/rfc6749#section-5.1
-//         console.log('token value ' + token.access_token);
-//         console.log('token will expire in (seconds) ' + token.expires_in);
-//         console.log('optional refresh token ' + token.refresh_token);
+//         // CREATE Post sample
+//         let response = await fetch(serverUrl + '/api/v1/posts', {
+//             method: 'POST', headers: authHeader, body: JSON.stringify({
+//                 data: {
+//                     type: 'posts',
+//                     attributes: {
+//                         title: 'New title 12345',
+//                         text: 'Some text',
+//                     }
+//                 }
+//             })
+//         });
+//         console.log('New post created at: ' + JSON.stringify(response.headers.get('Location')));
 //
-//         const apiUrl: string = apiPrefix + (new QueryBuilder('users'))
+//         // UPDATE Post sample
+//         await fetch(serverUrl + '/api/v1/posts', {
+//             method: 'PATCH', headers: authHeader, body: JSON.stringify({
+//                 data: {
+//                     id: '101',
+//                     type: 'posts',
+//                     attributes: {
+//                         title: 'Udpated title',
+//                     }
+//                 }
+//             })
+//         });
+//
+//         // SEARCH Posts sample
+//         const apiUrl: string = serverUrl + '/api/v1' + (new QueryBuilder('posts'))
 //             .withFilters({
-//                 field: 'first-name',
+//                 field: 'title',
 //                 operation: 'like',
-//                 parameters: '%a%'
-//             }, {
-//                 field: 'role',
-//                 operation: 'equals',
-//                 parameters: 'user'
+//                 parameters: '%12345%'
 //             })
 //             .withSorts({
-//                 field: 'last-name',
+//                 field: 'text',
 //                 isAscending: false
 //             })
 //             .withPagination(0, 10)
 //             .index();
+//         response = await fetch(apiUrl, {method: 'GET', headers: authHeader});
+//         console.log(await response.json());
 //
-//         let requestParams: RequestInit = {
-//             method: 'GET',
-//             headers: {
-//                 Authorization: 'Bearer ' + token.access_token,
-//             }
-//         };
-//
-//         const response = await fetch(apiUrl, requestParams);
-//         const json = await response.json();
-//
-//         console.log(json.data);
+//         // DELETE Post sample
+//         await fetch(serverUrl + '/api/v1/posts/101', {method: 'DELETE', headers: authHeader});
 //
 //     } catch (error) {
 //         if (error.reason !== undefined) {
