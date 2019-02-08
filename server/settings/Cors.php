ae2ce78575b082ffc439f1999737b32273773509
neomerx <info@neomerx.com>
2019-02-08 08:24:00 +0300

    Update to newer v2 CORS lib.

--- server/settings/Cors.php
+++ server/settings/Cors.php
@@ -19,32 +19,17 @@ class Cors extends CorsSettings
                 static::KEY_LOG_IS_ENABLED => filter_var(getenv('APP_ENABLE_LOGS'), FILTER_VALIDATE_BOOLEAN),
 
                 /**
-                 * A list of allowed request origins (lower-cased, no trail slashes).
-                 * Value `true` enables and value `null` disables origin.
-                 * If all origins '*' are enabled all settings for other origins are ignored.
+                 * A list of allowed request origins (no trail slashes, no default ports).
                  * For example, [
-                 *     'http://example.com:123'       => true,
-                 *     'http://evil.com'              => null,
-                 *     static::VALUE_ALLOW_ORIGIN_ALL => null,
+                 *     'http://example.com:123',
                  * ];
                  */
                 static::KEY_ALLOWED_ORIGINS => [
-                    static::VALUE_ALLOW_ORIGIN_ALL => true,
+                    'http://localhost:8080',
                 ],
 
                 /**
-                 * A list of allowed request methods (case sensitive).
-                 * Value `true` enables and value `null` disables method.
-                 *
-                 * For example,
-                 *
-                 * [
-                 *     'GET'    => true,
-                 *     'PATCH'  => true,
-                 *     'POST'   => true,
-                 *     'PUT'    => null,
-                 *     'DELETE' => true,
-                 * ];
+                 * A list of allowed request methods.
                  *
                  * Security Note: you have to remember CORS is not access control system and you should not expect all
                  * cross-origin requests will have pre-flights. For so-called 'simple' methods with so-called 'simple'
@@ -56,23 +41,15 @@ class Cors extends CorsSettings
                  * You can read more on 'simple' methods at http://www.w3.org/TR/cors/#simple-method
                  */
                 static::KEY_ALLOWED_METHODS => [
-                    'GET'    => true,
-                    'POST'   => true,
-                    'PATCH'  => true,
-                    'PUT'    => true,
-                    'DELETE' => true,
+                    'GET',
+                    'POST',
+                    'PATCH',
+                    'PUT',
+                    'DELETE',
                 ],
 
                 /**
-                 * A list of allowed request headers (lower-cased).
-                 * Value `true` enables and value `null` disables header.
-                 *
-                 * For example,
-                 *
-                 * $allowedHeaders = [
-                 *     'content-type'            => true,
-                 *     'x-custom-request-header' => null,
-                 * ];
+                 * A list of allowed request headers.
                  *
                  * Security Note: you have to remember CORS is not access control system and you should not expect all
                  * cross-origin requests will have pre-flights. For so-called 'simple' methods with so-called 'simple'
@@ -84,27 +61,21 @@ class Cors extends CorsSettings
                  * You can read more on 'simple' headers at http://www.w3.org/TR/cors/#simple-header
                  */
                 static::KEY_ALLOWED_HEADERS => [
-                    'accept'        => true,
-                    'content-type'  => true,
-                    'authorization' => true,
-                    'origin'        => true,
+                    'Accept',
+                    'Content-Type',
+                    'Authorization',
+                    'Origin',
                 ],
 
                 /**
                  * A list of headers (case insensitive) which will be made accessible to
-                 * user agent (browser) in response. Value `true` enables and value `null` disables header.
-                 *
-                 * For example,
-                 *
-                 * [
-                 *     'Content-Type'             => true,
-                 *     'X-Custom-Response-Header' => true,
-                 *     'X-Disabled-Header'        => null,
-                 * ];
+                 * user agent (browser) in response.
                  */
                 static::KEY_EXPOSED_HEADERS => [
-                    'content-type' => true,
+                    'Content-Type',
                 ],
+
+                static::KEY_IS_CHECK_HOST => !filter_var(getenv('APP_IS_DEBUG'), FILTER_VALIDATE_BOOLEAN),
 
             ] + parent::getSettings();
     }
